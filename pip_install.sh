e2f0a6a696ed8c9148d9c70995c4e3085e675c11
Avi Yaeli <avi.yaeli@gmail.com>
2018-12-04 16:45:43 +0200

    Create pip_install.sh

---
+++ pip_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PIP=/home/common/conda/anaconda2/bin/pip
+
+$PIP install PyMySQL==0.8.1
+$PIP install pathlib==1.0.1
+$PIP install mysql-connector==2.1.6
+$PIP install SQLAlchemy==1.1.13
+$PIP install textblob==0.15.1
+$PIP install redis==2.10.6
+$PIP install cx-Oracle==7.0.0
